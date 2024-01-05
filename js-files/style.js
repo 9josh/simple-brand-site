ad438f220642eff0acca56b46a9073201cbbeca8
Josh King <ceojosh00118@gmail.com>
2024-01-05 07:12:08 -0800

    adding the contact.html and finishing the styling, also adjustment of the navigation padding for all pages. adding js.files

---
+++ js-files/style.js
@@ -0,0 +1,13 @@
+// const body = document.querySelector('.body');
+const hero = document.querySelector('.hero-content h1');
+const contents = document.querySelector('.contents');
+const heroLists = document.querySelector('.hero-lists');
+
+window.addEventListener("load", () => {
+    hero.classList.add('loaded');
+    heroLists.classList.add('hero-load');
+})
+
+
+    
+
